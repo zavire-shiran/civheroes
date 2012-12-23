67dea0cd8d3549b3ae0fdb9b7e812769fe09a4d9
Zavire Shiran <zavire.shiran@gmail.com>
2012-12-23 01:48:10 -0600

    Initial commit.

---
+++ color_vertex.shader
@@ -0,0 +1,16 @@
+#version 330
+
+uniform mat4 WorldToCameraTransform;
+uniform mat4 CameraToClipTransform;
+
+layout(location=0) in vec2 position;
+layout(location=1) in vec4 in_color;
+
+smooth out vec4 color;
+
+void main()
+{
+  gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+  //gl_Position = CameraToClipTransform * WorldToCameraTransform * pos;
+  color = in_color;
+}
---
+++ world.py
@@ -0,0 +1,103 @@
+from OpenGL.GL import *
+import numpy
+from ctypes import c_void_p
+
+currentworld = None
+screensize = None
+
+
+def setscreensize(size):
+    global screensize
+    screensize = size
+
+
+def getworld():
+    global currentworld
+    return currentworld
+
+
+def transitionto(world):
+    global currentworld
+    currentworld = world(currentworld)
+
+
+def createshader(filename, shadertype):
+    shader = glCreateShader(shadertype)
+    source = open(filename).read()
+    glShaderSource(shader, source)
+    glCompileShader(shader)
+    ok = glGetShaderiv(shader, GL_COMPILE_STATUS)
+    if not ok:
+        print 'Shader compile failed:', filename
+        print glGetShaderInfoLog(shader)
+        # this should probably be an exception, but I can't be arsed right now.
+        return -1
+    return shader
+
+
+def createprogram(*shaders):
+    program = glCreateProgram()
+    for shader in shaders:
+        glAttachShader(program, shader)
+    glLinkProgram(program)
+    ok = glGetProgramiv(program, GL_LINK_STATUS)
+    if not ok:
+        print 'Could not link program:'
+        print glGetProgramInfoLog(program)
+        return -1
+    return program
+
+
+class World:
+    def __init__(self, previous = None):
+        pass
+
+    def keydown(self, key):
+        pass
+
+    def keyup(self, key):
+        pass
+
+    def click(self, pos):
+        pass
+
+    def draw(self):
+        pass
+
+    def step(self, dt):
+        pass
+
+rawbufferdata = [0, 0,   1, 0, 0, 1,
+                 0, 0.5, 0, 1, 0, 1,
+                 0.5, 0, 0, 0, 1, 1]
+bufferdata = numpy.array(rawbufferdata, numpy.float32)
+
+class Game(World):
+    def __init__(self, previous = None):
+        vertshader = createshader('color_vertex.shader', GL_VERTEX_SHADER)
+        fragshader = createshader('color_fragment.shader', GL_FRAGMENT_SHADER)
+        self.shaderprogram = createprogram(vertshader, fragshader)
+
+        self.world_to_camera_uniform = glGetUniformLocation(self.shaderprogram, 'WorldToCameraTransform')
+        self.camera_to_clip_uniform = glGetUniformLocation(self.shaderprogram, 'CameraToClipTransform')
+
+        self.vertexbuffer = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertexbuffer)
+        glBufferData(GL_ARRAY_BUFFER, bufferdata, GL_STATIC_DRAW)
+
+    def draw(self):
+        glUseProgram(self.shaderprogram)
+
+        identitymatrix = [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]
+
+        glUniformMatrix4fv(self.world_to_camera_uniform, 1, False, identitymatrix)
+        glUniformMatrix4fv(self.camera_to_clip_uniform, 1, False, identitymatrix)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertexbuffer)
+        glEnableVertexAttribArray(0)
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 6*4, None)
+        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 6*4, c_void_p(8))
+
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+
